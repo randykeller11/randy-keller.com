1653d958dfb8f53d9b61ea021f49cc9f8c805b80
Randy <randykeller1193@gmail.com>
2022-01-12 12:39:29 -0500

    editor colors setup

--- src/pages/ProductEditor.js
+++ src/pages/ProductEditor.js
@@ -6,6 +6,10 @@ import useAppStore from "../stores/useAppStore";
 function ProductEditor({ scene, isTransBack, isTransForward }) {
   const AppStore = useAppStore();
 
+  const firstRowColors = ["lightBlue", "orange", "gold", "red"];
+
+  const secondRowColors = ["purple", "green", "blue", "brown"];
+
   return (
     <AnimatePresence>
       {AppStore.scene === 1 &&
@@ -40,7 +44,24 @@ function ProductEditor({ scene, isTransBack, isTransForward }) {
             animate={{ opacity: 0.8 }}
             exit={{ opacity: 0 }}
             transition={{ type: "spring", stiffness: 500 }}
-          ></motion.div>
+          >
+            <div className="colorCard__row">
+              {firstRowColors.map((color) => (
+                <div
+                  className="colorCard__row__button"
+                  style={{ "background-color": color }}
+                ></div>
+              ))}
+            </div>
+            <div className="colorCard__row">
+              {secondRowColors.map((color) => (
+                <div
+                  className="colorCard__row__button"
+                  style={{ "background-color": color }}
+                ></div>
+              ))}
+            </div>
+          </motion.div>
         )}
     </AnimatePresence>
   );
